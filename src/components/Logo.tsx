75305e5db1d16e5f6757d1a9fbb0c5c20016143f
mluimes <mario.luis.mesa2004@gmail.com>
2024-11-01 21:12:31 +0100

    Logo gap fix

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,7 +8,7 @@ export function Logo({color}: LogoProps) {
     const colorName = color === 'blue' ? 'primary' : 'offwhite';
 
     return (
-        <a href="/" className='flex flex-row items-center'>
+        <a href="/" className='flex flex-row items-center gap-1'>
             <CloudSvg color={ colorName } />
             <p className={`text-[32px] text-${colorName}`}>HowsAir</p>
         </a>
