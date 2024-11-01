05a797edb0ae007a9f7d0353fbd93669c6d6c97d
mluimes <mario.luis.mesa2004@gmail.com>
2024-11-01 21:02:36 +0100

    tailwind.config updated
    added primary colors and dropshadow for header

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ export default {
       },
       colors: {
         primary: "#1074E7",
+        offwhite: "#F5F5F5",
+        offblack: "#112233",
+        gray: "#E3E3E3"
+      },
+      dropShadow: {
+        'header': '0 4px 4px rgba(16, 116, 231, 0.15)',
       },
     },
   },
