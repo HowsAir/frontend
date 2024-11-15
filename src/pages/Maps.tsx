8865a9658267eb3c94bc98fbb83bab6590cf430b
mluimes <mario.luis.mesa2004@gmail.com>
2024-11-15 17:59:10 +0100

    Mapas mejorado

--- src/pages/Maps.tsx
+++ src/pages/Maps.tsx
@@ -1,5 +1,11 @@
 const Maps = () => {
-    return <h2>Mapas</h2>;
+    return (
+        <div className="w-fit mx-auto text-center">
+            <h2 className="w-full">Mapas</h2>
+            <p>Lo siento, todavia estamos trabajando en esto...</p>
+            <img className="mt-8 mx-auto size-96" src="https://media0.giphy.com/media/H1dxi6xdh4NGQCZSvz/200w.gif?cid=6c09b9520oljohov3k5p47xyfvxcsv4qr30gyahx5ky7gwp7&ep=v1_gifs_search&rid=200w.gif&ct=g" alt="Gato trabajando"></img>
+        </div>
+    );
 };
 
 export default Maps;
