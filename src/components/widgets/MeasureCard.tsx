dc30ae6b31a1e98dde6a44d50a07574e3f9f89e4
mluimes <mario.luis.mesa2004@gmail.com>
2024-12-04 22:24:19 +0100

    Feat: MeasureCard updated
    OverallQuality with icons and colors, decided by a switch

--- src/components/widgets/MeasureCard.tsx
+++ src/components/widgets/MeasureCard.tsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
+const goodQualityIcon = '../../../public/icons/good-quality.svg';
+const regularQualityIcon = '../../../public/icons/regular-quality.svg';
+const toxicQualityIcon = '../../../public/icons/toxic-quality.svg';
 
 interface MeasureCardProps {
     value: number;
@@ -6,6 +9,7 @@ interface MeasureCardProps {
     title: string;
     type?: string;
     slider?: boolean;
+    average?: string;
 }
 
 export const MeasureCard: React.FC<MeasureCardProps> = ({
@@ -14,6 +18,7 @@ export const MeasureCard: React.FC<MeasureCardProps> = ({
     type,
     slider,
     title,
+    average,
 }) => {
     const [color, setColor] = useState('green');
     const gradientRef = useRef<HTMLDivElement | null>(null);
@@ -48,19 +53,58 @@ export const MeasureCard: React.FC<MeasureCardProps> = ({
         }
     }, [value]);
 
+    const overallQuality = {
+        icon: (() => {
+            switch (average) {
+                case 'Buena':
+                    return goodQualityIcon;
+                case 'Regular':
+                    return regularQualityIcon;
+                case 'Peligrosa':
+                    return toxicQualityIcon;
+                default:
+                    return;
+            }
+        })(),
+        color: (() => {
+            switch (average) {
+                case 'Buena':
+                    return '#49B500';
+                case 'Regular':
+                    return '#e6d200';
+                case 'Peligrosa':
+                    return '#E10000';
+                default:
+                    return;
+            }
+        })(),
+    };
+    
     return (
         <div className="flex min-h-52 w-fit min-w-80 flex-col rounded-lg border-[1px] border-gray bg-white px-6 py-4">
-            <div className="inline-flex gap-2">
+            <div className="inline-flex justify-between">
                 <div className="flex flex-col">
-                    <p className="text-lg font-medium">{title}</p>
-                    <label className="text-lg font-normal leading-none text-neutral-500">
+                    <p className="text-lg font-medium">
+                        {title}{'  '}
+                        <button className="size-6 rounded-md bg-gray">i</button>
+                    </p>
+                    <label className="text-lg font-normal leading-none">
                         {date}
                     </label>
+                </div>
+                <div className={`flex flex-col items-center ${average == null ? 'hidden' : ''}`}>
+                    <p className="text-base text-neutral-500 font-light">Media hoy</p>
+                    <img
+                        className="size-6 bg-green-50"
+                        alt="Img media"
+                        src={overallQuality.icon}
+                    ></img>
+                    <p className='text-base -mt-1' style={{ color: overallQuality.color }}>{average}</p>
                 </div>
             </div>
             <h1 className="m-auto flex w-fit text-offblack">
                 {value}
-                <p className="text-3xl mb-2.5 ml-1 mt-auto">{type}</p>
+                <p className="mb-2.5 ml-1 mt-auto text-3xl">{type}</p>
             </h1>
             <div className={`inline-flex gap-2 ${slider ? '' : 'opacity-0'}`}>
                 0
