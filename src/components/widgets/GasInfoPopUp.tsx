41763714cfd7fe31ec269736a273cdd785acb828
Boregxrd <manuelborregales12@gmail.com>
2024-12-12 22:18:18 +0100

    fix: added limit per hour values for each gas on gasInfoPopUp

--- src/components/widgets/GasInfoPopUp.tsx
+++ src/components/widgets/GasInfoPopUp.tsx
@@ -5,8 +5,8 @@ interface GasInfoPopUpProps {
 export const GasInfoPopUp = ({ togglePopup }: GasInfoPopUpProps) => {
     return (
         <div className="fixed inset-0 z-40 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="relative mx-24 h-fit w-[70%] rounded-lg bg-offwhite px-12 py-8 mt-20">
-                <h2 className="w-full text-center text-3xl mb-4">
+            <div className="relative mx-24 mt-20 h-fit w-[70%] rounded-lg bg-offwhite px-12 py-8">
+                <h2 className="mb-4 w-full text-center text-3xl">
                     Contaminantes y consejos
                 </h2>
                 <button
@@ -21,12 +21,18 @@ export const GasInfoPopUp = ({ togglePopup }: GasInfoPopUpProps) => {
                 <div className="h-fit w-full rounded-lg bg-white p-8">
                     <table className="w-full">
                         <thead>
-                            <th className="pb-4"></th>
-                            <th className="pb-4">Dióxido de Nitrógeno (NO2)</th>
-                            <th className="pb-4">Ozono (O3)</th>
-                            <th className="pb-4">Monóxido de carbono (CO)</th>
+                            <th className="pb-2"></th>
+                            <th className="pb-2">Dióxido de Nitrógeno (NO2)</th>
+                            <th className="pb-2">Ozono (O3)</th>
+                            <th className="pb-2">Monóxido de carbono (CO)</th>
                         </thead>
-                        <tbody>
+                        <tr className="text-center text-base font-normal text-[#1652A0]">
+                            <td className="pb-2"></td>
+                            <td className="pb-2">Límite/hora: 200 µg/m³</td>
+                            <td className="pb-2">Límite/hora: 180 μg/m³</td>
+                            <td className="pb-2">Límite/hora: 30 mg/m³</td>
+                        </tr>
+                        <tbody className="mt-4">
                             <tr className="text-left font-light">
                                 <td className="border-r-2 border-gray pr-4">
                                     <img
@@ -37,7 +43,8 @@ export const GasInfoPopUp = ({ togglePopup }: GasInfoPopUpProps) => {
                                 </td>
                                 <td className="border-r-2 border-gray px-4">
                                     <span className="font-medium">Origen:</span>{' '}
-                                    Tráfico y combustibles fósiles.
+                                    Tráfico y combustibles fósiles, residuos
+                                    industriales.
                                 </td>
                                 <td className="border-r-2 border-gray px-4">
                                     <span className="font-medium">Origen:</span>{' '}
@@ -103,12 +110,16 @@ export const GasInfoPopUp = ({ togglePopup }: GasInfoPopUpProps) => {
                             </tr>
                         </tbody>
                     </table>
-                    <p className="mt-8 mb-2 font-medium">Consejos generales</p>
-                    <ul className="list-disc marker:text-primary grid grid-cols-2 w-1/2 mx-auto gap-2 text-left font-light">
+                    <p className="mb-2 mt-8 text-center font-medium">
+                        Consejos generales
+                    </p>
+                    <ul className="mx-auto grid w-1/2 list-disc grid-cols-2 gap-2 text-left font-light marker:text-primary">
                         <li className="w-fit">Usa purificadores en casa</li>
                         <li className="w-fit">Ventila temprano o tarde</li>
-                        <li className="w-fit">Opta por rutas con menos tráfico</li>
-                        <li className="w-fit">Usa mascarrila en zonas de mucha contaminación</li>
+                        <li className="w-fit">Opta por rutas sin tráfico</li>
+                        <li className="w-fit">
+                            Usa mascarrila en zonas de mucha contaminación
+                        </li>
                     </ul>
                 </div>
             </div>
