6d593f669a0e464088f4318069df32e57de0c675
mluimes <mario.luis.mesa2004@gmail.com>
2025-01-17 03:44:32 +0100

    feat: MapComponent certain timestamps use

--- src/components/layouts/MapComponent.tsx
+++ src/components/layouts/MapComponent.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
 import { GasInfoPopUp } from '../widgets/GasInfoPopUp';
 import { getCurrentAirQualityMap } from '../../api/apiClient';
+import { AirQualityMap } from '../../api/data';
 
 interface MapProps {
-    date?: Date | null;
+    data?: AirQualityMap | null;
     portal?: boolean;
 }
 
-export const MapComponent = ({ portal, date }: MapProps) => {
+export const MapComponent = ({ portal, data }: MapProps) => {
     const [gasInfoPopUp, setGasInfoPopUp] = useState<boolean>(false);
     const [htmlContent, setHtmlContent] = useState('');
     const iframeRef = useRef<HTMLIFrameElement>(null);
@@ -17,7 +18,7 @@ export const MapComponent = ({ portal, date }: MapProps) => {
     };
 
     useEffect(() => {
-        if (date === null) {
+        if (data == null) {
             const fetchAirQualityMap = async () => {
                 try {
                     // Fetch the map URL using the API method
@@ -31,6 +32,7 @@ export const MapComponent = ({ portal, date }: MapProps) => {
                         );
                     }
                     const html = await response.text();
+                    console.log(html);
                     setHtmlContent(html);
                 } catch (error) {
                     console.error('Failed to fetch air quality map:', error);
@@ -38,8 +40,27 @@ export const MapComponent = ({ portal, date }: MapProps) => {
             };
 
             fetchAirQualityMap();
-        } else {}
-    }, [date]); // Dependency on date
+        } else {
+            const fetchHtmlContent = async () => {
+                try {
+                    if (data) {
+                        const response = await fetch(data.url);
+                        if (!response.ok) {
+                            throw new Error(
+                                `Error fetching HTML: ${response.statusText}`
+                            );
+                        }
+                        const html = await response.text();
+                        setHtmlContent(html);
+                    }
+                } catch (error) {
+                    console.error('Failed to fetch HTML content:', error);
+                }
+            };
+
+            fetchHtmlContent();
+        }
+    }, [data]); // Dependency on data
 
     useEffect(() => {
         if (iframeRef.current && htmlContent) {
@@ -62,7 +83,7 @@ export const MapComponent = ({ portal, date }: MapProps) => {
                 <iframe
                     ref={iframeRef}
                     title="Dynamic HTML"
-                    className={` mb-0 h-full w-full rounded-md border-0`}
+                    className={`mb-0 h-full w-full rounded-md border-0`}
                 />
 
                 <button className="absolute bottom-20 right-4 rounded-md bg-white p-2 transition-all duration-200 ease-in-out hover:bg-neutral-200">
