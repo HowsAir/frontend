a6d3bff058ba014cc07ad3da6bb9670901ae38a6
mluimes <mario.luis.mesa2004@gmail.com>
2025-01-17 03:30:18 +0100

    feat: date handling after select done
    now i have to modify MapComponent to allow map from certain timestamps

--- src/components/layouts/AdminMaps.tsx
+++ src/components/layouts/AdminMaps.tsx
@@ -2,29 +2,46 @@ import { useState } from 'react';
 import { MapComponent } from './MapComponent';
 import SelectDateBtn from '../common/SelectDateBtn';
 import DatePicker from '../widgets/DatePicker';
+import { getHistoricalAirQualityMap } from '../../api/apiClient';
+import type { AirQualityMap } from '../../api/data';
 
 export const AdminMaps = () => {
-    const [showDatePicker, setShowDatePicker] = useState(true);
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const [mapData, setMapData] = useState<AirQualityMap | null>(null);
 
     const toggleDatePicker = () => {
         setShowDatePicker(!showDatePicker);
     };
 
-    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-
-    const handleDateSelect = (date: Date) => {
-        setSelectedDate(date);
-        setShowDatePicker(false);
+    const handleDateSelect = async (timestamp: string) => {
+        console.log(timestamp);
+        setIsLoading(true);
+        setError(null);
+        try {
+            const data = await getHistoricalAirQualityMap(timestamp);
+            setMapData(data);
+        } catch (err) {
+            setError('Error fetching map data');
+            console.error(err);
+        } finally {
+            setIsLoading(false);
+        }
     };
 
     return (
         <div className="relative">
-            <h3>Mapas</h3>
+            <h3>{`Mapas:`}</h3>
 
-            {/* Pass the toggleChooseDayPopUp function to SelectDateBtn */}
             <SelectDateBtn onClick={toggleDatePicker} />
 
-            <MapComponent />
+            {isLoading && <div>Cargando...</div>}
+            {!isLoading && mapData && <p>Fecha seleccionada: {new Date(mapData.timestamp).toLocaleDateString()}</p>}
+            {error && <div className="text-red-500">{error}</div>}
+            {}
+
+            <MapComponent  />
 
             {showDatePicker && (
                 <DatePicker
